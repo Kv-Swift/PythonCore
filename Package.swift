4cd490e23a30e385af4c75e290f94d58023dfd1e
kivyswiftlink <kivyswiftlink@gmail.com>
2024-08-28 17:22:45 +0000

    Update from https://github.com/kivyswiftlink/CoreBuilder/commit/a465c52528cb6f6077dda9fedb5c09b68fc5acd6

--- Package.swift
+++ Package.swift
@@ -30,10 +30,7 @@ let package = Package(
     	.target(
         	name: "PythonCore", 
         	dependencies: [
-        		"libssl", 
-        		"libcrypto", 
-        		"libpython311", 
-        		"libffi", 
+        		"xcframework", 
         	], 
         	resources: [
         	], 
@@ -54,9 +51,6 @@ let package = Package(
         	linkerSettings: [
         	]
     	), 
-    	.binaryTarget(name: "libssl", url: "https://github.com/KivySwiftLink/PythonCore/releases/download/311.0.1/libssl.zip", checksum: "0664613cc07ce5c0c6fad63abfd2510d70388fbe5f34ee3cd35ff5eec827e759"), 
-    	.binaryTarget(name: "libcrypto", url: "https://github.com/KivySwiftLink/PythonCore/releases/download/311.0.1/libcrypto.zip", checksum: "8b674067234022d4f2426863beb9d570550a4774290a79a9518258c6c2e17ba5"), 
-    	.binaryTarget(name: "libpython311", url: "https://github.com/KivySwiftLink/PythonCore/releases/download/311.0.1/libpython311.zip", checksum: "43aa81fe114fa6c793e9aff43dcd5ed00be68e385468b49c6a244c174a8842b1"), 
-    	.binaryTarget(name: "libffi", url: "https://github.com/KivySwiftLink/PythonCore/releases/download/311.0.1/libffi.zip", checksum: "eaa65f4eb547725f1967cdc467ab6894be0b6d8b7bf2857c256ac116ac68f74b"), 
+    	.binaryTarget(name: "xcframework", url: "https://github.com/KivySwiftLink/PythonCore/releases/download/311.0.0/xcframework", checksum: ""), 
     ]
 )
