0f283768e3b3c37a7e4ac29ec36bb7278c82956d
kivyswiftlink <kivyswiftlink@gmail.com>
2024-09-05 11:14:50 +0000

    Update from https://github.com/kivyswiftlink/CoreBuilder/commit/1618f743f8cf0a35583ed7e54b5fdf31b15de32d

--- Package.swift
+++ Package.swift
@@ -47,7 +47,7 @@ let package = Package(
     	.target(
         	name: "PythonExtra", 
         	dependencies: [
-        		.product(name: "Python"), 
+        		"Python"
         	], 
         	resources: [
         	], 
@@ -64,9 +64,9 @@ let package = Package(
         	linkerSettings: [
         	]
     	), 
-    	.binaryTarget(name: "libssl", url: "https://github.com/KivySwiftLink/PythonCore/releases/download/311.0.0/libssl.zip", checksum: "caa70a91f65714aad735dcaf8fa1b9b83f430838861ad22c3c0ed1c58a55af8f"), 
-    	.binaryTarget(name: "libcrypto", url: "https://github.com/KivySwiftLink/PythonCore/releases/download/311.0.0/libcrypto.zip", checksum: "e90f9a8a1654c02784278f91b499fc0ee0e01a08e2fdb791265575442e115390"), 
-    	.binaryTarget(name: "libpython311", url: "https://github.com/KivySwiftLink/PythonCore/releases/download/311.0.0/libpython311.zip", checksum: "bd1c8b0c77181933af41d601d5b0c9484d0c99227e9e72ca899c8753825f709a"), 
-    	.binaryTarget(name: "libffi", url: "https://github.com/KivySwiftLink/PythonCore/releases/download/311.0.0/libffi.zip", checksum: "b0a8427a166b684199c3fe2836a63943960ce9b5564206e6b8830a496567f435"), 
+    	.binaryTarget(name: "libssl", url: "https://github.com/KivySwiftLink/PythonCore/releases/download/311.0.0/libssl.zip", checksum: "e1735f071798a147834633fd80ada12c1db35da8db888a2865535f845a5d4c35"), 
+    	.binaryTarget(name: "libcrypto", url: "https://github.com/KivySwiftLink/PythonCore/releases/download/311.0.0/libcrypto.zip", checksum: "3036e35553f29ded130516bf2217a67e095b383afb9cdf838a9192572c56f80e"), 
+    	.binaryTarget(name: "libpython311", url: "https://github.com/KivySwiftLink/PythonCore/releases/download/311.0.0/libpython311.zip", checksum: "0b8286a61ee94e428322bcc19a57f20039959f630bd004acc6525d07f87c0ea5"), 
+    	.binaryTarget(name: "libffi", url: "https://github.com/KivySwiftLink/PythonCore/releases/download/311.0.0/libffi.zip", checksum: "3f78a51a13507c00556612aeaad7e6f082b2ed8fde151a05cd99573d6d16715c"), 
     ]
 )
