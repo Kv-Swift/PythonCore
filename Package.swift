21270b55d303a9b3415449da02f96e1f4164f773
kivyswiftlink <kivyswiftlink@gmail.com>
2024-09-07 12:16:24 +0000

    Update from https://github.com/kivyswiftlink/CoreBuilder/commit/074e18c5c6405d0c4488a45d5038648ec9c87dd7

--- Package.swift
+++ Package.swift
@@ -60,14 +60,14 @@ let package = Package(
         	dependencies: [
         	], 
         	resources: [
-        	.copy("lib.zip")
+        	.copy("lib")
         	], 
         	linkerSettings: [
         	]
     	), 
-    	.binaryTarget(name: "libssl", url: "https://github.com/KivySwiftLink/PythonCore/releases/download/311.0.0/libssl.zip", checksum: "1cb4fd75fa88b64ddd083ef0691e523fa5ace0b8247f4a84922568d148ff51db"), 
-    	.binaryTarget(name: "libcrypto", url: "https://github.com/KivySwiftLink/PythonCore/releases/download/311.0.0/libcrypto.zip", checksum: "36e6dd751003f3da18c7af44c2468d43604d3861dd9b6cbe89fa1daa8a27b0e7"), 
-    	.binaryTarget(name: "libpython311", url: "https://github.com/KivySwiftLink/PythonCore/releases/download/311.0.0/libpython311.zip", checksum: "a6338fb939817b380c31c33e6c595f5fedfb138e7e038e917f8ce20e31486846"), 
-    	.binaryTarget(name: "libffi", url: "https://github.com/KivySwiftLink/PythonCore/releases/download/311.0.0/libffi.zip", checksum: "9c406c04426f0998c35b4d9c5e79459ab82f0e530fd67ea87ef0b9374e1fc083"), 
+    	.binaryTarget(name: "libssl", url: "https://github.com/KivySwiftLink/PythonCore/releases/download/311.0.0/libssl.zip", checksum: "b69de71134ebccf6180a1529c6329e918e4836b83962ebd7c6787fa745f73fc2"), 
+    	.binaryTarget(name: "libcrypto", url: "https://github.com/KivySwiftLink/PythonCore/releases/download/311.0.0/libcrypto.zip", checksum: "fc614f520795216d7379ff5c57ec6223ebfb30bc324cf54c067d7dedb8ebeb71"), 
+    	.binaryTarget(name: "libpython311", url: "https://github.com/KivySwiftLink/PythonCore/releases/download/311.0.0/libpython311.zip", checksum: "ff9814dc8e4f4d435a25b0d48f2ad18f3b0c4e7faacbc6f07931c529778c88a8"), 
+    	.binaryTarget(name: "libffi", url: "https://github.com/KivySwiftLink/PythonCore/releases/download/311.0.0/libffi.zip", checksum: "b93ee61cc1c4e2ba82baee0cb57fc0c74ccd43108b02b7a7112fb2be0c2cbd54"), 
     ]
 )
