9b58f4067365e177e48ab8c7658516583a676ad8
kivyswiftlink <kivyswiftlink@gmail.com>
2024-10-12 17:04:18 +0000

    Update from https://github.com/kivyswiftlink/CoreBuilder/commit/86a8df243525c29308e02400d946f26db270d7ee

--- Package.swift
+++ Package.swift
@@ -1,73 +1,73 @@
 
-// swift-tools-version: 5.8
+// swift-tools-version: 5.9
 
 import PackageDescription
 
 let package = Package(
-    name: "PythonCore", 
-    platforms: [.iOS(.v13)], 
+    name: "PythonCore",
+    platforms: [.iOS(.v13)],
     products: [
     	.library(
-        	name: "PythonCore", 
+        	name: "PythonCore",
         	targets: [
-        		"PythonCore", 
-        "libpython311", 
-        "libssl", 
-        "libcrypto", 
+        		"PythonCore",
+        "libpython311",
+        "libssl",
+        "libcrypto",
         "libffi"
         	]
-    	), 
+    	),
     	.library(
-        	name: "PythonLibrary", 
+        	name: "PythonLibrary",
         	targets: [
         		"PythonLibrary"
         	]
-    	), 
-    ], 
+    	),
+    ],
     dependencies: [
-    ], 
-    targets: [
+    ], targets: [
     	.target(
-        	name: "PythonCore", 
+        	name: "PythonCore",
         	dependencies: [
-        		"libssl", 
-        		"libcrypto", 
-        		"libpython311", 
-        		"libffi", 
-        		"PythonExtra", 
-        	], 
+        		"libssl",
+        		"libcrypto",
+        		"libpython311",
+        		"libffi",
+        		"PythonExtra",
+        	],
         	resources: [
-        	], 
+        	],
         	linkerSettings: [
-        		.linkedLibrary("bz2"), 
-        		.linkedLibrary("z"), 
-        		.linkedLibrary("ncurses"), 
-        		.linkedLibrary("sqlite3"), 
+        		.linkedLibrary("bz2"),
+        		.linkedLibrary("z"),
+        		.linkedLibrary("ncurses"),
+        		.linkedLibrary("sqlite3"),
         	]
-    	), 
+    	),
     	.target(
-        	name: "PythonExtra", 
+        	name: "PythonExtra",
         	dependencies: [
-        		"libpython311", 
-        	], 
+        		"libpython311",
+        	],
         	resources: [
-        	], 
+        	],
         	linkerSettings: [
         	]
-    	), 
+    	),
     	.target(
-        	name: "PythonLibrary", 
+        	name: "PythonLibrary",
         	dependencies: [
-        	], 
+        	],
         	resources: [
         	.copy("lib")
-        	], 
+        	],
         	linkerSettings: [
         	]
-    	), 
-    	.binaryTarget(name: "libssl", url: "https://github.com/KivySwiftLink/PythonCore/releases/download/311.0.1/libssl.zip", checksum: "eae99d74b0a0058f15a3627cf67f0c842b38f5ab2b53fd4a60474cb3d84213b7"), 
-    	.binaryTarget(name: "libcrypto", url: "https://github.com/KivySwiftLink/PythonCore/releases/download/311.0.1/libcrypto.zip", checksum: "a42379cfbc6fbe8f66279271a8cccee791b702fa022fb304843e1bd2b8f854ed"), 
-    	.binaryTarget(name: "libpython311", url: "https://github.com/KivySwiftLink/PythonCore/releases/download/311.0.1/libpython311.zip", checksum: "c4d6bd91b1a4448fb5bcfba3f50d72bc626b74ca53d8409e88e0f60e0b2f5bea"), 
-    	.binaryTarget(name: "libffi", url: "https://github.com/KivySwiftLink/PythonCore/releases/download/311.0.1/libffi.zip", checksum: "d63c76eceb6278f462070071a7756af5dd46d35f53fbe780b16fab23b80b2fd2"), 
+    	),
+    	.binaryTarget(name: "libssl", url: "https://github.com/KivySwiftLink/PythonCore/releases/download/0.0.10/libssl.zip", checksum: "013bff97047839d9d4373185cc4b4d0aa79e716a0fe7c861b78800734ed3f2ac"),
+    	.binaryTarget(name: "libcrypto", url: "https://github.com/KivySwiftLink/PythonCore/releases/download/0.0.10/libcrypto.zip", checksum: "877ce0f8b723b4fca07c64b224d282cf863e9f6934977526f7ef5e0055bb1d1d"),
+    	.binaryTarget(name: "libpython311", url: "https://github.com/KivySwiftLink/PythonCore/releases/download/0.0.10/libpython311.zip", checksum: "f14f010c9b203374a8dbe12099473e501f35feda7d0716c41d7fbc35196bc62c"),
+    	.binaryTarget(name: "libffi", url: "https://github.com/KivySwiftLink/PythonCore/releases/download/0.0.10/libffi.zip", checksum: "533fd5e6b8031557a9b2b85dba89b1a4c23c9d7f4cff2092f75216db9eb75220"),
     ]
+
 )
