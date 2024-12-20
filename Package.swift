3c582e7763ac310116c8b5e383a358203f78e0d1
kivyswiftlink <kivyswiftlink@gmail.com>
2024-12-20 21:35:35 +0000

    Update from https://github.com/kivyswiftlink/CoreBuilder/commit/6ac70b219087f126103c6c54d23fccc7c31b7d85

--- Package.swift
+++ Package.swift
@@ -50,23 +50,23 @@ let package = Package(
         ),
         .binaryTarget(
             name: "libpython3.11",
-            url: "https://github.com/kivyswiftlink/PythonCore/releases/download/311.0.5/libpython3.11.zip",
-            checksum: "78814b9e3c5e2f284321e466f0896383768a82c8c5d8c2235d1159ad7597b74e"
+            url: "https://github.com/kivyswiftlink/PythonCore/releases/download/311.0.7/libpython3.11.zip",
+            checksum: "97cd0bac208da3ba2f2a5b69e8ba8605aa0662c79add0f38b4572bcd4009ba6c"
         ),
         .binaryTarget(
             name: "libssl",
-            url: "https://github.com/kivyswiftlink/PythonCore/releases/download/311.0.5/libssl.zip",
-            checksum: "6c350a282e616573f1a9c5cb7881296460ab5f83210c38a31959d7b969e95025"
+            url: "https://github.com/kivyswiftlink/PythonCore/releases/download/311.0.7/libssl.zip",
+            checksum: "b9d7f01344898cbce483ff92eb09016ddd9709b28119950c696cbc8e5a05f3a5"
         ),
         .binaryTarget(
             name: "libcrypto",
-            url: "https://github.com/kivyswiftlink/PythonCore/releases/download/311.0.5/libcrypto.zip",
-            checksum: "03e5285a431cab07215bc69111d3f789bbd95d9f4309d527401213a70fa836a1"
+            url: "https://github.com/kivyswiftlink/PythonCore/releases/download/311.0.7/libcrypto.zip",
+            checksum: "92951db4c9391431538fe582d9e5223910eb757f291577a0defd682e4ff0c8f6"
         ),
         .binaryTarget(
             name: "libffi",
-            url: "https://github.com/kivyswiftlink/PythonCore/releases/download/311.0.5/libffi.zip",
-            checksum: "bb7ee2294dc643859c3ad9b172dc439d4d2d1d093c791aa981ae74fdf9492230"
+            url: "https://github.com/kivyswiftlink/PythonCore/releases/download/311.0.7/libffi.zip",
+            checksum: "67f769a5f2ae1ac18ddc041baa61d2d2735b1afb6ae67fc483c1af8a06e09d77"
         )
     ]
 )
