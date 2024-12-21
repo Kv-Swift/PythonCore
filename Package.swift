a7a3901494348e6e3a904cd1f106a786ec5663a3
kivyswiftlink <kivyswiftlink@gmail.com>
2024-12-21 09:43:44 +0000

    Update from https://github.com/kivyswiftlink/CoreBuilder/commit/6ac70b219087f126103c6c54d23fccc7c31b7d85

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     name: "PythonCore",
     platforms: [.iOS(.v13)],
     products: [
-        .library(name: "PythonCore", targets: ["PythonCore"])
+        .library(name: "PythonCore", targets: ["PythonCore"]), .library(name: "PythonLibrary", targets: ["PythonLibrary"])
     ],
     dependencies: [
     ],
@@ -50,23 +50,23 @@ let package = Package(
         ),
         .binaryTarget(
             name: "libpython3.11",
-            url: "https://github.com/kivyswiftlink/PythonCore/releases/download/311.0.10/libpython3.11.zip",
-            checksum: "247c351b206a6ae5f69725d5cd5947f1de0b830ed82de2471f9e48951a7a9531"
+            url: "https://github.com/kivyswiftlink/PythonCore/releases/download/311.0.11/libpython3.11.zip",
+            checksum: "792869fdc867eaadee0ee6d671eaff0020c3bf3900ae9d150a468068f9c06b3d"
         ),
         .binaryTarget(
             name: "libssl",
-            url: "https://github.com/kivyswiftlink/PythonCore/releases/download/311.0.10/libssl.zip",
-            checksum: "4a4ec118fa322f4f2fb59ef837037b204447369d606bdc479741e9a2d3d16f47"
+            url: "https://github.com/kivyswiftlink/PythonCore/releases/download/311.0.11/libssl.zip",
+            checksum: "ce1562eb7a793d621f0665da9ef47f844e61b1d5237d42b1424432a749a5db65"
         ),
         .binaryTarget(
             name: "libcrypto",
-            url: "https://github.com/kivyswiftlink/PythonCore/releases/download/311.0.10/libcrypto.zip",
-            checksum: "78601a55df77ebb5b918459955cf1c2885fa2580a4ce5bae735a3be188ca3495"
+            url: "https://github.com/kivyswiftlink/PythonCore/releases/download/311.0.11/libcrypto.zip",
+            checksum: "33797ed6f11aee8cb24b896ffe182a08f721a7d1197902bd54f2762d058c71fc"
         ),
         .binaryTarget(
             name: "libffi",
-            url: "https://github.com/kivyswiftlink/PythonCore/releases/download/311.0.10/libffi.zip",
-            checksum: "c8eebe57085b116afda02c3a09dfa1810959926ca913c667be9aaf4e03b450ab"
+            url: "https://github.com/kivyswiftlink/PythonCore/releases/download/311.0.11/libffi.zip",
+            checksum: "2989abc6145dd61a7d97eff82a40aba57c41b3664715e71953c19899f5bb0ac2"
         )
     ]
 )
