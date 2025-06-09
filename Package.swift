bac54848521cb7602842b39b72093eac0a276b8e
kv-swift <kivyswiftlink@gmail.com>
2025-06-09 22:09:19 +0000

    Update from https://github.com/Kv-Swift/CoreBuilder/commit/a9ef29a0c0ad8c2e321e3995803eb8ca1eb7b6e0

--- Package.swift
+++ Package.swift
@@ -42,23 +42,23 @@ let package = Package(
         ),
         .binaryTarget(
             name: "libpython3.11",
-            url: "https://github.com/kv-swift/PythonCore/releases/download/311.1.17/libpython3.11.zip",
-            checksum: "9953c1033564e47c365417aba0681ab43f16e0333f9f0ab4849881a4913ef74c"
+            url: "https://github.com/kv-swift/PythonCore/releases/download/311.1.18/libpython3.11.zip",
+            checksum: "382dc59da96e73d756560c6eb701831e6dff45766048efa02db817c3da9c1f85"
         ),
         .binaryTarget(
             name: "libssl",
-            url: "https://github.com/kv-swift/PythonCore/releases/download/311.1.17/libssl.zip",
-            checksum: "a7b5c147c839b39bc6f746da724ebeb8626b4071c1c3af7374e57fc2f0e0385f"
+            url: "https://github.com/kv-swift/PythonCore/releases/download/311.1.18/libssl.zip",
+            checksum: "4233a920eca615fd75a757a541748cf5b19ad122b26be0a40a715c8401dfecca"
         ),
         .binaryTarget(
             name: "libcrypto",
-            url: "https://github.com/kv-swift/PythonCore/releases/download/311.1.17/libcrypto.zip",
-            checksum: "b8f236a93c40a67a8a212ca0ff4145ef6caffea400f53315dd9eee483b0303e9"
+            url: "https://github.com/kv-swift/PythonCore/releases/download/311.1.18/libcrypto.zip",
+            checksum: "860b7d116844e5e54719925a7b215d631423de02a51dc1c999243e14c9ea888c"
         ),
         .binaryTarget(
             name: "libffi",
-            url: "https://github.com/kv-swift/PythonCore/releases/download/311.1.17/libffi.zip",
-            checksum: "673140e28fa95885c8f5131964e716b964c2ee68f63fb56b593e9812b085d595"
+            url: "https://github.com/kv-swift/PythonCore/releases/download/311.1.18/libffi.zip",
+            checksum: "0bc3bed2a1e64dcee2939cbc3f9cea51c8c1f5ea66ec7881cfd37a4597ef54f8"
         )
     ]
 )
