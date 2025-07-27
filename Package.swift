4be758fd2e45bda6a7426e7a3ab95ab4211b7356
kv-swift <kivyswiftlink@gmail.com>
2025-07-27 14:38:04 +0000

    Update from https://github.com/Kv-Swift/CoreBuilder/commit/a9ef29a0c0ad8c2e321e3995803eb8ca1eb7b6e0

--- Package.swift
+++ Package.swift
@@ -42,23 +42,23 @@ let package = Package(
         ),
         .binaryTarget(
             name: "libpython3.11",
-            url: "https://github.com/kv-swift/PythonCore/releases/download/311.1.20/libpython3.11.zip",
-            checksum: "f808fb79717143175a6be3f43a80079202fdf60f94e1b4d521fdd7a7426368ec"
+            url: "https://github.com/kv-swift/PythonCore/releases/download/311.1.21/libpython3.11.zip",
+            checksum: "35495d05897fc65407dc2937f8772117c39378eb0b2b4cf6eea8c89ad5981150"
         ),
         .binaryTarget(
             name: "libssl",
-            url: "https://github.com/kv-swift/PythonCore/releases/download/311.1.20/libssl.zip",
-            checksum: "a5601464fa5ffb168f44220f7ee80303891a57e836eea10c9cc071b37994d587"
+            url: "https://github.com/kv-swift/PythonCore/releases/download/311.1.21/libssl.zip",
+            checksum: "111d99bb3856069c43d37359ffd1d5867763ee90c6884daa46c8e1bbef663fac"
         ),
         .binaryTarget(
             name: "libcrypto",
-            url: "https://github.com/kv-swift/PythonCore/releases/download/311.1.20/libcrypto.zip",
-            checksum: "e99503c5e475c27ba59d0c4fefcb0997e903c537ba0f40d4031c333bf177f439"
+            url: "https://github.com/kv-swift/PythonCore/releases/download/311.1.21/libcrypto.zip",
+            checksum: "088286f3f0e9bbdc12faf061be93564860f01a2179b216e5faf875389fcffe90"
         ),
         .binaryTarget(
             name: "libffi",
-            url: "https://github.com/kv-swift/PythonCore/releases/download/311.1.20/libffi.zip",
-            checksum: "ee963e4d7e59bb46494d3736db630f80b3ed7c45151fab672d6a1c1d44349015"
+            url: "https://github.com/kv-swift/PythonCore/releases/download/311.1.21/libffi.zip",
+            checksum: "b9e9244df34f6d5974323dcbd0b8b7e20d4a8846bad49146570d8e65593f4824"
         )
     ]
 )
